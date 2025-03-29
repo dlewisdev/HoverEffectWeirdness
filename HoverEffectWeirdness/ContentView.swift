e79c0a6af10b03f6769c07576423f7e59c517210
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2025-03-29 14:06:58 -0400

    Initial Commit

---
+++ HoverEffectWeirdness/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  HoverEffectWeirdness
+//
+//  Created by Danielle Lewis on 3/29/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ HoverEffectWeirdness/HoverEffectWeirdnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoverEffectWeirdnessApp.swift
+//  HoverEffectWeirdness
+//
+//  Created by Danielle Lewis on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct HoverEffectWeirdnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
