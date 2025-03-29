0146358e1cd532aee53229d76500ff0511da7329
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2025-03-29 15:56:47 -0400

    recreate DisclosureGroup hoverEffect issue

--- HoverEffectWeirdness/ContentView.swift
+++ HoverEffectWeirdness/ContentView.swift
@@ -11,14 +11,70 @@ import RealityKitContent
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Model3D(named: "Scene", bundle: realityKitContentBundle)
-                .padding(.bottom, 50)
-
-            Text("Hello, world!")
+        ScrollView {
+            DisclosureGroup {
+                VStack(alignment: .listRowSeparatorLeading) {
+                    ForEach(0..<5) { _ in
+                        childView
+                    }
+                }
+                
+            } label: {
+                parentView
+                    .padding(10)
+                    .contentShape(.hoverEffect, .rect)
+                    .hoverEffect()
+            }
         }
         .padding()
     }
+    
+    var childView: some View {
+        ScrollView {
+            HStack {
+                Button {
+                    // does nothing
+                } label: {
+                    HStack {
+                        Image(systemName: "person.fill.turn.left")
+                            .resizable()
+                            .frame(width: 75, height: 50)
+                        
+                        Spacer()
+                        
+                        Text("There is no capsule here")
+                    }
+                    .padding()
+                    .contentShape(.hoverEffect, .rect)
+                    .hoverEffect()
+                }
+                .buttonStyle(.plain)
+                .padding()
+                
+            }
+            .padding()
+        }
+    }
+    
+    var parentView: some View {
+        HStack(alignment: .lastTextBaseline) {
+            Image(systemName: "person.fill.questionmark")
+                .resizable()
+                .frame(width: 125, height: 100)
+            Spacer()
+            
+            VStack(alignment: .trailing) {
+                Text("There's a capsule hoverEffect")
+                Text("On the outer edges")
+                Text("Of this parent view")
+            }
+            .font(.title)
+            
+        }
+        .padding()
+    }
+    
+    
 }
 
 #Preview(windowStyle: .automatic) {
